fe6fd83539fa869d13b6658e91d94a117a682b39
nirav-jivani <niravjivani82@gmail.com>
2021-01-31 19:16:31 +0530

    updated

--- Aadhar_application/Aadhar_application/settings.py
+++ Aadhar_application/Aadhar_application/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Aadhar_application.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,10 +75,10 @@ WSGI_APPLICATION = 'Aadhar_application.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    #'default': {
+     #   'ENGINE': 'django.db.backends.sqlite3',
+      #  'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 }
 
 
@@ -118,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+	os.path.join(BASE_DIR,'static')
+	]
+STATIC_ROOT = os.path.join(BASE_DIR,'imageFile')
+
+
---
+++ Aadhar_application/application/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+
+#class Details(request):
+#	//first_name=VarCharField();
+	
